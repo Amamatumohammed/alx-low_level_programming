d5f60a29129cd103db2f643edf419e61523e47a9
Amamatumohammed <amamatumohammedsalis328@gmail.com>
2022-12-12 13:19:05 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a c program using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with a proper grammer, but without a piece of art,/n");
+	return (0);
+
+}
