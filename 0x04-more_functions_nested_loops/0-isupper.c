ed7855589aa8e75d5f85065fc96684b8f9c57d02
Amamatumohammed <amamatumohammedsalis328@gmail.com>
2022-12-15 03:48:14 -0800

    0-isupper.c git commit -m 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checkes upper case
+@c: print int
+* Return: give 1 or 0
+*/
+
+ int _i supper(int c)
+{	
+if (c ›= 'A' && c <=  'z')
+{	
+return (1);
+}
+
+else
+{
+return (O);
+}
+
+}
